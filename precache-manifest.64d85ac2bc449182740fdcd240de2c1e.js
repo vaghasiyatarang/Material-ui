e6bc53433831bfde07af42d6df12c499ff21a3f1
Miral26 <miral.goyani84@gmail.com>
2022-11-25 12:08:32 +0530

    Updates

---
+++ precache-manifest.64d85ac2bc449182740fdcd240de2c1e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e4df8c3cd2df9216da7",
+    "url": "/Material-ui/static/js/0.8e4df8c3.chunk.js"
+  },
+  {
+    "revision": "96b2bbf8848cd8bb07c8",
+    "url": "/Material-ui/static/js/1.96b2bbf8.chunk.js"
+  },
+  {
+    "revision": "a9dc9f454bd6e422fd07",
+    "url": "/Material-ui/static/js/2.a9dc9f45.chunk.js"
+  },
+  {
+    "revision": "af208333493f8d42ec2b",
+    "url": "/Material-ui/static/css/main.4f82ff48.chunk.css"
+  },
+  {
+    "revision": "af208333493f8d42ec2b",
+    "url": "/Material-ui/static/js/main.af208333.chunk.js"
+  },
+  {
+    "revision": "99ef524b13d76f2281cf",
+    "url": "/Material-ui/static/js/4.99ef524b.chunk.js"
+  },
+  {
+    "revision": "ccb4fdd78abe8ea38f4e",
+    "url": "/Material-ui/static/js/5.ccb4fdd7.chunk.js"
+  },
+  {
+    "revision": "a61e0cc1272729ef14b0",
+    "url": "/Material-ui/static/js/6.a61e0cc1.chunk.js"
+  },
+  {
+    "revision": "8562aa87b903d550732a",
+    "url": "/Material-ui/static/js/7.8562aa87.chunk.js"
+  },
+  {
+    "revision": "634c12b007d3d1222f9a",
+    "url": "/Material-ui/static/js/8.634c12b0.chunk.js"
+  },
+  {
+    "revision": "b2c50bafe8b3faaed39b",
+    "url": "/Material-ui/static/js/9.b2c50baf.chunk.js"
+  },
+  {
+    "revision": "6311210ed90fac7fddba",
+    "url": "/Material-ui/static/js/10.6311210e.chunk.js"
+  },
+  {
+    "revision": "de5a2c4e33ede6c431f1",
+    "url": "/Material-ui/static/js/11.de5a2c4e.chunk.js"
+  },
+  {
+    "revision": "3f311a7f88aaf70fe851",
+    "url": "/Material-ui/static/js/12.3f311a7f.chunk.js"
+  },
+  {
+    "revision": "00effbce2710806674b8",
+    "url": "/Material-ui/static/js/runtime~main.00effbce.js"
+  },
+  {
+    "revision": "4b3e889e6bf8578c45b3fa88e58d369f",
+    "url": "/Material-ui/index.html"
+  }
+];
